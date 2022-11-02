21e2475154f852c3013d37174939dd887b908183
vasyl-sw14 <77231350+vasyl-sw14@users.noreply.github.com>
2022-11-02 13:11:29 +0200

    initial animation

--- client/lib/components/node.dart
+++ client/lib/components/node.dart
@@ -1,23 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
 
-class Node extends StatelessWidget {
+class Node extends StatefulWidget {
   const Node({required this.value, required this.onTap, super.key});
 
   final int value;
   final VoidCallback onTap;
 
   @override
+  State<Node> createState() => _NodeState();
+}
+
+class _NodeState extends State<Node> {
+  SMIBool? _node;
+
+  void _onRiveInit(Artboard artboard) {
+    final controller = StateMachineController.fromArtboard(
+        artboard, 'State Machine 1',
+        onStateChange: (machine, event) {});
+    artboard.addController(controller!);
+    _node = controller.findInput<bool>('PRessed') as SMIBool;
+  }
+
+  void _hitNode() => _node?.change(true);
+
+  @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-          height: 50,
-          width: 50,
-          alignment: Alignment.center,
-          margin: const EdgeInsets.all(5),
-          decoration:
-              const BoxDecoration(color: Colors.red, shape: BoxShape.circle),
-          child: Text(value.toString())),
-    );
+    return Container(
+        height: 50,
+        width: 50,
+        alignment: Alignment.center,
+        margin: const EdgeInsets.all(5),
+        child: GestureDetector(
+          onTap: () {
+            _hitNode();
+          },
+          child: ClipRRect(
+              borderRadius: BorderRadius.circular(50),
+              child: RiveAnimation.asset(
+                'node.riv',
+                fit: BoxFit.cover,
+                onInit: _onRiveInit,
+              )),
+        ));
   }
 }
