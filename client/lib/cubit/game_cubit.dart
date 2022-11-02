ccabda4915df1a301d55fa6a33cb15475a99e39c
vasyl-sw14 <77231350+vasyl-sw14@users.noreply.github.com>
2022-11-02 16:03:10 +0200

    added sound

--- client/lib/cubit/game_cubit.dart
+++ client/lib/cubit/game_cubit.dart
@@ -15,10 +15,6 @@ final LiveQuery liveQuery = LiveQuery();
 class GameCubit extends Cubit<GameState> {
   GameCubit() : super(GameState.empty());
 
-  final AudioPlayer player = AudioPlayer();
-
-  static const String filePath = 'sound.mp3';
-
   final _authService = getIt<AuthService>();
   final _roomService = getIt<RoomService>();
   final _gameEventService = getIt<GameEventService>();
@@ -55,6 +51,8 @@ class GameCubit extends Cubit<GameState> {
   }
 
   void tapNode(int y, int x) async {
+    await AudioPlayer().play(AssetSource('sound.mp3'));
+
     updateNode(y, x, _authService.user!.objectId!);
     await _gameEventService.create(room!.objectId!, y, x);
   }
